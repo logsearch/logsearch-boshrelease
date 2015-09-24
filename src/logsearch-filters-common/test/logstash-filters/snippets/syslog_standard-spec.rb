6c0081c62aa39cac9c13412095d4eeadaaa9f55a
David Laing <david@davidlaing.com>
2015-09-24 20:37:56 +0000

    Populate @source.host with sensible value

--- src/logsearch-filters-common/test/logstash-filters/snippets/syslog_standard-spec.rb
+++ src/logsearch-filters-common/test/logstash-filters/snippets/syslog_standard-spec.rb
@@ -11,10 +11,12 @@ describe LogStash::Filters::Grok do
   CONFIG
 
   describe "Accepting standard syslog message without PID specified" do
-    sample("@ingestor" => { "received_from" => "1.2.3.4:12345" } , "@message" => '<85>Apr 24 02:05:03 localhost sudo: bosh_h5156e598 : TTY=pts/0 ; PWD=/var/vcap/bosh_ssh/bosh_h5156e598 ; USER=root ; COMMAND=/bin/pwd') do
+    sample("@ingestor" => { "received_from" => "1.2.3.4" } , "@message" => '<85>Apr 24 02:05:03 localhost sudo: bosh_h5156e598 : TTY=pts/0 ; PWD=/var/vcap/bosh_ssh/bosh_h5156e598 ; USER=root ; COMMAND=/bin/pwd') do
       insist { subject["tags"] } == [ 'syslog_standard' ]
       insist { subject["@timestamp"] } == Time.iso8601("#{Time.now.year}-04-24T02:05:03.000Z")
+      
       insist { subject['@source']['host'] } == '1.2.3.4'
+      insist { subject['syslog_hostname'] } == 'localhost'
 
       insist { subject['syslog_facility'] } == 'security/authorization'
       insist { subject['syslog_facility_code'] } == 10
@@ -30,7 +32,9 @@ describe LogStash::Filters::Grok do
     sample("@ingestor" => { "received_from" => "1.2.3.4" }, "@message" => '<78>Apr 24 04:03:06 localhost crontab[32185]: (root) LIST (root)') do
       insist { subject["tags"] } == [ 'syslog_standard' ]
       insist { subject["@timestamp"] } == Time.iso8601("#{Time.now.year}-04-24T04:03:06.000Z")
+      
       insist { subject['@source']['host'] } == '1.2.3.4'
+      insist { subject['syslog_hostname'] } == 'localhost'
 
       insist { subject['syslog_facility'] } == 'clock'
       insist { subject['syslog_facility_code'] } == 9
