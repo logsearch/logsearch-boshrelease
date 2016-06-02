9afa0f3b462e854ed8482b0326fa4546b5fc03e3
Derik Evangelista <devangelista@pivotal.io>
2016-06-02 17:54:18 +0100

    Add redact_passwords filter
    for aws credentials
    
    [#120771947]
    
    Signed-off-by: Bence Monus <bmonus@pivotal.io>

---
+++ src/logsearch-config/test/logstash-filters/snippets/redact_passwords-spec.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+require 'test/filter_test_helpers'
+
+describe "Redact passwords filter" do
+  before(:all) do
+    load_filters <<-CONFIG
+      filter {
+        #{File.read("src/logstash-filters/snippets/redact_passwords.conf")}
+      }
+    CONFIG
+  end
+
+  context "when parsing logs with AWS credentials in it" do
+    when_parsing_log(
+      "@message" => '{"timestamp":"1464361200.410290003","source":"ServiceBackup","message":"ServiceBackup.Running command: \u0026{Path:/var/vcap/packages/aws-cli/bin/aws Args:[/var/vcap/packages/aws-cli/bin/aws --endpoint-url https://s3-eu-west-1.amazonaws.com s3 sync /var/vcap/store/parser/logs-to-be-archived s3://logsearch-backup/turkish/2016/05/27] Env:[AWS_ACCESS_KEY_ID=AKIAJKCO5PJBSKTRUSJR AWS_SECRET_ACCESS_KEY=AivaiZa3ooh2eeGahngoo8ahp2uikei2NooC9Kie]}'
+    ) do
+
+      it "adds the redacted tag" do
+	expect(subject["tags"]).to include "redacted"
+      end
+
+      it "redacts the access key id" do
+	expect(subject["@message"]).to match /AWS_ACCESS_KEY_ID=AKI\*{6}/
+	expect(subject["@message"]).to_not match /AWS_ACCESS_KEY_ID=AKIAJKCO5PJBSKTRUSJR/
+      end
+
+      it "redacts the secret access key" do
+	expect(subject["@message"]).to match /AWS_SECRET_ACCESS_KEY=Aiv\*{6}/
+	expect(subject["@message"]).to_not match /AWS_ACCESS_KEY_ID=AivaiZa3ooh2eeGahngoo8ahp2uikei2NooC9Kie/
+      end
+    end
+  end
+end
