3f8c2fde7afdbbf418fedb6ce581dc4f63991a5d
Ada Solcan <asolcan@pivotal.io>
2016-06-10 10:49:43 +0100

    More explicit test credentials
    [#121134863]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>

--- src/logsearch-config/test/logstash-filters/snippets/redact_passwords-spec.rb
+++ src/logsearch-config/test/logstash-filters/snippets/redact_passwords-spec.rb
@@ -12,7 +12,7 @@ describe "Redact passwords filter" do
 
   context "when parsing logs with AWS credentials in it" do
     when_parsing_log(
-      "@message" => '{"timestamp":"1464361200.410290003","source":"ServiceBackup","message":"ServiceBackup.Running command: \u0026{Path:/var/vcap/packages/aws-cli/bin/aws Args:[/var/vcap/packages/aws-cli/bin/aws --endpoint-url https://s3-eu-west-1.amazonaws.com s3 sync /var/vcap/store/parser/logs-to-be-archived s3://logsearch-backup/turkish/2016/05/27] Env:[AWS_ACCESS_KEY_ID=AKIAJKCO5PJBSKTRUSJR AWS_SECRET_ACCESS_KEY=AivaiZa3ooh2eeGahngoo8ahp2uikei2NooC9Kie]}'
+      "@message" => '{"timestamp":"1464361200.410290003","source":"ServiceBackup","message":"ServiceBackup.Running command: \u0026{Path:/var/vcap/packages/aws-cli/bin/aws Args:[/var/vcap/packages/aws-cli/bin/aws --endpoint-url https://s3-eu-west-1.amazonaws.com s3 sync /var/vcap/store/parser/logs-to-be-archived s3://logsearch-backup/turkish/2016/05/27] Env:[AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY]}'
     ) do
 
       it "adds the redacted tag" do
@@ -21,12 +21,12 @@ describe "Redact passwords filter" do
 
       it "redacts the access key id" do
 	expect(subject["@message"]).to match /AWS_ACCESS_KEY_ID=AKI\*{6}/
-	expect(subject["@message"]).to_not match /AWS_ACCESS_KEY_ID=AKIAJKCO5PJBSKTRUSJR/
+	expect(subject["@message"]).to_not match /AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE/
       end
 
       it "redacts the secret access key" do
 	expect(subject["@message"]).to match /AWS_SECRET_ACCESS_KEY=Aiv\*{6}/
-	expect(subject["@message"]).to_not match /AWS_ACCESS_KEY_ID=AivaiZa3ooh2eeGahngoo8ahp2uikei2NooC9Kie/
+	expect(subject["@message"]).to_not match "AWS_ACCESS_KEY_ID=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
       end
     end
   end
